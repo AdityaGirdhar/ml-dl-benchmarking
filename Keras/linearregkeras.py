c420c45ee5fb926896361944405bf217a23db991
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2023-06-11 00:05:26 +0530

    correct file without classes till now

--- Keras/linearregkeras.py
+++ Keras/linearregkeras.py
@@ -2,13 +2,21 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from tensorflow.keras import layers
-from sklearn.metrics import mean_squared_error
 
-# Load the data and create the DataFrame
 cols = ["frequency", "angleofattack", "chordlength", "freestreamvelocity", "suctionsidedisplacement", "soundpressure"]
-data = np.loadtxt("/content/airfoil_self_noise.dat")
+data = np.loadtxt("airfoil_self_noise.dat")
+#visuaize data
 df = pd.DataFrame(data, columns=cols)
+for label in df.columns[:-1]:
+    plt.scatter(df[label], df["soundpressure"])
+    plt.title(label)
+    plt.ylabel("Pressure of sound")
+    plt.xlabel(label)
+    plt.show()
 
 # Separate the input features (X) and target variable (y)
 X = df.drop("soundpressure", axis=1)
@@ -21,40 +29,38 @@ test = df.drop(train.index)
 # Normalize the input features
 X_mean = X.mean()
 X_std = X.std()
-X_train = (train.drop("soundpressure", axis=1) - X_mean) / X_std
-y_train = train["soundpressure"]
-X_test = (test.drop("soundpressure", axis=1) - X_mean) / X_std
-y_test = test["soundpressure"]
+X = (X - X_mean) / X_std
 
-# Define the linear regression model using Keras
+# Build the linear regression model using Keras
 model = keras.Sequential()
-model.add(layers.Dense(1, input_shape=(X_train.shape[1],)))
+model.add(layers.Dense(1, input_shape=(X.shape[1],)))
 
 # Define the SGD optimizer with learning rate
-optimizer = keras.optimizers.SGD(learning_rate=0.01)
+optimizer = keras.optimizers.SGD(lr=0.01)
 
-# Compile the model
-model.compile(optimizer=optimizer, loss='mean_squared_error')
+# Compile the model with the optimizer and loss function
+model.compile(optimizer=optimizer, loss="mean_squared_error")
 
-# Training loop
-num_epochs = 10
-display_step = 100
-for epoch in range(num_epochs):
-    # Forward pass
-    with tf.GradientTape() as tape:
-        pred = model(X_train, training=True)
-        loss_value = model.loss(y_train, pred)
-    
-    # Calculate gradients
-    gradients = tape.gradient(loss_value, model.trainable_variables)
-    
-    # Update weights and bias
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    
-    # Print loss every display_step epochs
-    if (epoch + 1) % display_step == 0:
-        print("Epoch:", '%04d' % (epoch + 1), "loss=", "{:.9f}".format(loss_value))
+# Train the model
+model.fit(X, y, epochs=1000, batch_size=32)
 
-mse = model.evaluate(X_test, y_test)
-print("Mean Squared Error:", mse)
+# Evaluate the model on the test set
+X_test = (test.drop("soundpressure", axis=1) - X_mean) / X_std
+y_test = test["soundpressure"]
+y_pred = model.predict(X_test).reshape(-1)  # Reshape to 1-dimensional array
 
+mse = keras.metrics.mean_squared_error(y_test, y_pred)
+mae = keras.metrics.mean_absolute_error(y_test, y_pred)
+
+# Start a TensorFlow session
+with tf.Session() as sess:
+    mse_value = sess.run(mse)
+    mae_value = sess.run(mae)
+
+print("MSE:", mse_value)
+print("MAE:", mae_value)
+
+
+# plt.scatter(X_test,y_test, color= 'black')
+plt.scatter(y_test,model.predict(X_test),color= "blue",linewidth=3)
+plt.show()
