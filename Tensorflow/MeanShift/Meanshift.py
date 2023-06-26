cfeecb05bdf3a587115f3d95794ec24557b8882b
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2023-06-26 09:59:41 +0530

    mean shift done

---
+++ Tensorflow/MeanShift/Meanshift.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+# Loading image and pre-processing
+img = tf.keras.preprocessing.image.load_img('peppers.png', target_size=(150, 200))
+image = tf.keras.preprocessing.image.img_to_array(img)
+image = image.astype('float32') / 255.0
+
+# Getting the dimensions of the image and setting hyperparameters
+width = image.shape[0]
+height = image.shape[1]
+colors = image.shape[2]
+num_of_epochs = 10
+bandwidth = 0.1
+
+# Creating TensorFlow variables
+tf_image = tf.Variable(image, dtype=tf.float32)
+
+# Defining the training loop
+start = time.time()
+for epoch in range(num_of_epochs):
+    tf_image2 = tf_image.numpy().copy()
+    for w in range(width):
+        for h in range(height):
+            k = tf.exp(tf.reduce_sum(-tf.square((tf_image[w, h] - tf_image) / bandwidth), axis=2))
+            a = tf.reduce_sum((tf_image - tf_image[w, h]) * tf.reshape(k, (width, height, 1)), axis=(0, 1))
+            b = tf.reduce_sum(k, axis=(0, 1))
+            tf_image2[w, h] = tf_image[w, h] + a / b
+    tf_image.assign(tf_image2)
+end = time.time()
+
+# Training the model
+print(f"Time taken to train the model: {end-start} seconds")
+
+# # Displaying the generated image
+# generated_image = tf_image.numpy()
+# plt.imshow(generated_image)
+# plt.axis('off')
+# plt.show()
