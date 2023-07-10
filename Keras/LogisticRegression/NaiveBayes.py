b1f81adc061b96991f13fbaf3e86f2a685842d09
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2023-07-10 12:35:47 +0530

    did memory, time profile , accuracy testing , also added image proofs of same

--- Keras/LogisticRegression/NaiveBayes.py
+++ Keras/LogisticRegression/NaiveBayes.py
@@ -1,66 +1,91 @@
 import numpy as np
 import pandas as pd
-import time
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import SGD
+import cProfile
+import pstats
+from memory_profiler import profile
+
 class NaiveBayes:
     def __init__(self):
         self.classes = None
         self.model = None
-
+    @profile
     def fit(self, X_train, y_train):
-        st = time.time()
         self.classes = np.unique(y_train)
         num_classes = len(self.classes)
         num_features = X_train.shape[1]
 
         self.model = Sequential()
         self.model.add(Dense(num_classes, input_shape=(num_features,), activation='softmax'))
-        opt = SGD(learning_rate=0.01)  
-        self.model.compile(optimizer= opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        opt = SGD(learning_rate=0.01)
+        self.model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        profiler = cProfile.Profile()
+        profiler.enable()
+
         self.model.fit(X_train, y_train, epochs=10, verbose=0)
-        end_time = time.time()
-        print(end_time - st)
-        
+
+        profiler.disable()
+        stats = pstats.Stats(profiler)
+        stats.strip_dirs()
+        stats.sort_stats('tottime')
+        stats.print_stats('fit')
+    @profile
     def predict(self, X_test):
-        start_time = time.time()
+        profiler = cProfile.Profile()
+        profiler.enable()
+
         y_pred = np.argmax(self.model.predict(X_test), axis=1)
-        end_time = time.time()
-        prediction_time = end_time - start_time
-        return y_pred, prediction_time
 
-# Set GPU memory growth if available
-physical_devices = tf.config.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        profiler.disable()
+        stats = pstats.Stats(profiler)
+        stats.strip_dirs()
+        stats.sort_stats('tottime')
+        stats.print_stats('predict')
 
-# Load the dataset and split into training and testing sets
-cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
-df = pd.read_csv("magic04.data", names=cols)
-df["class"] = (df["class"] == "g").astype(int)
+        return y_pred
 
-X = df.drop("class", axis=1).values
-y = df["class"].values
 
-# Manual train-test split
-np.random.seed(42)
-shuffle_indices = np.random.permutation(len(X))
-train_indices = shuffle_indices[:int(0.8 * len(X))]
-test_indices = shuffle_indices[int(0.8 * len(X)):]
+def run_code():
+    # Set GPU memory growth if available
+    physical_devices = tf.config.list_physical_devices('GPU')
+    if len(physical_devices) > 0:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-X_train, X_test = X[train_indices], X[test_indices]
-y_train, y_test = y[train_indices], y[test_indices]
+    # Load the dataset and split into training and testing sets
+    cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+    df = pd.read_csv("magic04.data", names=cols)
+    df["class"] = (df["class"] == "g").astype(int)
 
-# Create and train the Naive Bayes classifier
-naive_bayes = NaiveBayes()
-naive_bayes.fit(X_train, y_train)
+    X = df.drop("class", axis=1).values
+    y = df["class"].values
 
-tup = naive_bayes.predict(X_test)
+    # Manual train-test split
+    np.random.seed(42)
+    shuffle_indices = np.random.permutation(len(X))
+    train_indices = shuffle_indices[:int(0.8 * len(X))]
+    test_indices = shuffle_indices[int(0.8 * len(X)):]
 
-# Calculate accuracy
-accuracy = np.mean(tup[0] == y_test)
+    X_train, X_test = X[train_indices], X[test_indices]
+    y_train, y_test = y[train_indices], y[test_indices]
 
-# print("Test Accuracy: {:.4f}".format(accuracy))
-# print("Time taken in predicting: {:.4f} seconds".format(tup[1]))
+    # Create and train the Naive Bayes classifier
+    naive_bayes = NaiveBayes()
+
+  
+    naive_bayes.fit(X_train, y_train)
+ 
+    y_pred = naive_bayes.predict(X_test)
+    
+    _, train_accuracy = naive_bayes.model.evaluate(X_train, y_train, verbose=0)
+    print("Training Accuracy: {:.4f}".format(train_accuracy))
+    
+    # Calculate accuracy
+    accuracy = np.mean(y_pred == y_test)
+    print("Test Accuracy: {:.4f}".format(accuracy))
+
+
+run_code()
--- Keras/LogisticRegression/logistickeras.py
+++ Keras/LogisticRegression/logistickeras.py
@@ -1,84 +1,91 @@
+from memory_profiler import profile
 import pandas as pd
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from tensorflow.keras.optimizers import SGD
-
+import cProfile
+import pstats
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-
-
 from tensorflow import keras
 import tensorflow as tf
 from tensorflow.keras import layers
-import time
-
-class LogisticRegression:
-    def __init__(self, input_dim):
-        self.model = keras.Sequential()
-        opti = SGD(learning_rate=0.01)  # Set the learning rate using 'lr' parameter
-        self.model.add(layers.Dense(1, activation='sigmoid', input_shape=(input_dim,)))
-        self.model.compile(optimizer=opti, loss='binary_crossentropy', metrics=['accuracy'])
-        
-
-    def fit(self, X_train, y_train, num_epochs, display_step):
-      start_time = time.time()
-      # start_time= time.time()
-      for epoch in range(num_epochs):
-            # Train the model for one epoch
-        history = self.model.fit(X_train, y_train, epochs=1, verbose=0)
-
-            # Display the loss every display_staep steps
-        if (epoch + 1) % display_step == 0:
-          loss = history.history['loss'][0]
-          # print("Epoch " + str(epoch+1) + ", Loss: " + str(loss))
-      endtime = time.time()
-      pt = endtime - start_time
-      print(pt)
-      
-
-    def predict(self, X_test):
-      start_time= time.time()
-      val = self.model.predict(X_test)
-      end_time= time.time()
-      train_time= end_time - start_time
-      return val , train_time
-    def evaluate(self, X_test, y_test):
-      _, accuracy = self.model.evaluate(X_test, y_test, verbose=0)
-      return accuracy
-
-physical_devices = tf.config.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
-cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
-df = pd.read_csv("magic04.data", names=cols)
-df["class"] = (df["class"] == "g").astype(int)
+# profiled memory , time profiler also done , divided data as well , train and test data also splited
+ 
+@profile
+def run_code():
+   
 
-train =  df.sample(frac=0.8) 
-test= df.sample(frac=0.2) 
+    class LogisticRegression:
+        def __init__(self, input_dim):
+            self.model = keras.Sequential()
+            opti = SGD(learning_rate=0.01)
+            self.model.add(layers.Dense(1, activation='sigmoid', input_shape=(input_dim,)))
+            self.model.compile(optimizer=opti, loss='binary_crossentropy', metrics=['accuracy'])
 
-train =  df.sample(frac=0.8) 
-test= df.sample(frac=0.2) 
+        def fit(self, X_train, y_train, num_epochs, display_step):
+            self.train_accuracy = []
+
+            profiler = cProfile.Profile()
+            profiler.enable()
+
+            for epoch in range(num_epochs):
+                history = self.model.fit(X_train, y_train, epochs=1, verbose=0)
+                self.train_accuracy.append(history.history['accuracy'][0])
+
+            profiler.disable()
+            stats = pstats.Stats(profiler)
+            stats.strip_dirs()
+            stats.sort_stats('tottime')
+            stats.print_stats('fit')
+
+        def predict(self, X_test):
+            profiler = cProfile.Profile()
+            profiler.enable()
+
+            val = self.model.predict(X_test)
+
+            profiler.disable()
+            stats = pstats.Stats(profiler)
+            stats.strip_dirs()
+            stats.sort_stats('tottime')
+            stats.print_stats('predict')
+
+            return val
+
+        def evaluate(self, X_test, y_test):
+            _, accuracy = self.model.evaluate(X_test, y_test, verbose=0)
+            return accuracy
+
+    physical_devices = tf.config.list_physical_devices('GPU')
+    if len(physical_devices) > 0:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+    df = pd.read_csv("magic04.data", names=cols)
+    df["class"] = (df["class"] == "g").astype(int)
+
+    train = df.sample(frac=0.8)
+    test = df.sample(frac=0.2)
+
+    X_train = train.drop("class", axis=1)
+    Y_train = train["class"]
+
+    X_test = test.drop("class", axis=1)
+    Y_test = test["class"]
+
+    logreg = LogisticRegression(X_train.shape[1])
+    logreg.fit(X_train, Y_train, 100, 10)
+
+    accuracy = logreg.evaluate(X_test, Y_test)
+    predictions = logreg.predict(X_test)
+
+    print("Test Accuracy:", accuracy)
+    print("Training Accuracy:", logreg.train_accuracy[-1])
 
 
-X_train = train.drop("class", axis=1)
-Y_train = train["class"]
-
-X_test = test.drop("class", axis=1)
-Y_test = test["class"]
-
-logreg= LogisticRegression(X_train.shape[1])
-pt=logreg.fit(X_train , Y_train, 100, 10)
-# print("training time" , pt)
-X = train.drop("class", axis=1)
-y = train["class"]
-
-tup = logreg.predict(X_test)
-
-accuracy = logreg.evaluate(X_test, Y_test)
-# print("Test Accuracy: " + str(accuracy))
-# print("predicting time " , tup[1])
+run_code()
